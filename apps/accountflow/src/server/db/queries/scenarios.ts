597765f677a04956b99094ddd0e27e454a210f1c
Hugo Gu <hugogu@outlook.com>
2026-02-03 10:43:31 +0800

    feat: Add scenarios query file with getScenario function

---
+++ apps/accountflow/src/server/db/queries/scenarios.ts
@@ -0,0 +1,9 @@
+import { db } from '../index'
+import { scenarios } from '../schema'
+import { eq } from 'drizzle-orm'
+
+export async function getScenario(id: number) {
+  return db.query.scenarios.findFirst({
+    where: eq(scenarios.id, id)
+  })
+}
